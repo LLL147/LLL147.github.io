886ba297d73854da64e480fb110d9258c6e9a9ba
LLL147 <a15053514652@outlook.com>
2025-01-04 22:27:46 +0800

    Site updated: 2025-01-04 22:27:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/31/hello-world/"];function toRandomPost(){
+var posts=["2025/01/04/alist/小雅alist/","2025/01/04/alist安装/","2024/12/31/Hexo使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
