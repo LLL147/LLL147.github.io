8a680efb5bae5a8213e7293d9f39676869a325c5
LLL147 <a15053514652@outlook.com>
2025-01-11 21:13:48 +0800

    Site updated: 2025-01-11 21:13:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/05/alist/alist安卓/","2025/01/05/alist/alistWindows/","2025/01/04/alist/小雅alist/","2025/01/04/alist安装/","2024/12/31/Hexo使用/"];function toRandomPost(){
+var posts=["2025/01/05/Hexo使用/","2025/01/05/alist/alist安卓/","2025/01/05/alist/alistWindows/","2025/01/04/alist/小雅alist/","2025/01/04/alist安装/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
